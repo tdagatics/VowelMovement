e265d80a1c3b93315b10029f7bbfe150a5d13ee6
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-03 12:13:16 -0700

    Updated VowelMovements with Block

--- VowelMovement/main.m
+++ VowelMovement/main.m
@@ -14,8 +14,25 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSArray *originalStrings = [[NSMutableArray alloc] init];
+        originalStrings = @[@"Sauerkraut", @"Raygun", @"Big Nerd Ranch", @"Mississippi"];
+        NSLog(@"The original strings are %@", originalStrings);
+        NSMutableArray *devowelizedStrings = [[NSMutableArray alloc] init];
+        NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
+       
+        // Declare the block variable
+        void (^devowelizer)(id, NSUInteger, BOOL *);
         
+        devowelizer = ^(id string, NSUInteger i, BOOL *stop) {
+            NSMutableString *newString = [NSMutableString stringWithString:string];
+            
+            for (NSString *s in vowels) {
+                NSRange fullRange = NSMakeRange(0, [newString length]);
+                [newString replaceOccurrencesOfString:s withString:@"" options:NSCaseInsensitiveSearch range:fullRange];
+            }
+            
+            [devowelizedStrings addObject:newString];
+        }; // End of lbock assignment
     }
     return 0;
 }
